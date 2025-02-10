4f12613d5455e12cbede387e556f53712ef4d9ef
AUDREY <159445383+thatswiftie@users.noreply.github.com>
2025-02-10 11:04:02 -0500

    Chessboard done

--- Session_1/src/ChessBoard/Chessboard.java
+++ Session_1/src/ChessBoard/Chessboard.java
@@ -1,4 +1,56 @@
 package ChessBoard;
 
-public class Chessboard {
+public class Chessboard
+{
+    private String[][] chessBoard = new String[8][8];
+    private String[] firstLine = {"Rook", "Knight", "Bishop", "Queen", "King", "Bishop", "Knight", "Rook"};
+    private String[] lastLine = {"Rook", "Knight", "Bishop", "Queen", "King", "Bishop", "Knight", "Rook"};
+    private int lastIdx = 7;
+
+    // Add constructor to initialize an empty board
+    public Chessboard()
+    {
+        for(int row = 0; row < 8; row++)
+        {
+            for(int col = 0; col < 8; col++)
+            {
+                chessBoard[row][col] = "----";
+            }
+        }
+    }
+
+    public String[][] fillBoard()
+    {
+        chessBoard[0] = firstLine;
+        chessBoard[lastIdx] = lastLine;
+
+        for(int row = 0; row < 8; row++)
+        {
+            if(row == 1 || row == 6)
+            {
+                for(int col = 0; col < 8; col++)
+                {
+                    chessBoard[row][col] = "Pawn";
+                }
+            }
+        }
+        return chessBoard;
+    }
+
+    public String[][] getChessBoard()
+    {
+        return chessBoard;
+    }
+
+    public void printChessboard()
+    {
+        for(int row = 0; row < 8; row++)
+        {
+            for(int col = 0; col < 8; col++)
+            {
+                System.out.print(chessBoard[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
--- Session_1/src/ChessBoard/Main.java
+++ Session_1/src/ChessBoard/Main.java
@@ -1,12 +1,46 @@
 package ChessBoard;
 
+import java.util.Scanner;
+
 public class Main
 {
     public static void main(String[] args)
     {
+        Scanner input = new Scanner(System.in);
+        Chessboard board = new Chessboard();
+        board.fillBoard();
+        boolean validPlace = false;
+        String player = "";
+        board.printChessboard();
+        while(!validPlace)
+        {
+            System.out.println("Enter the row of player");
+            int row = input.nextInt() - 1;
+            System.out.println("Enter the column of player");
+            int col = input.nextInt() - 1;
 
+            if(((row >= 0 && row < 2)||(row >= 6 && row < 8)) && (col >= 0 && col < 8))
+            {
+                player = board.getChessBoard()[row][col];
+                board.getChessBoard()[row][col] = "----";
+                validPlace = true;
+            }
+        }
+        validPlace = false;
+        while(!validPlace)
+        {
+            System.out.println("Enter the row destination");
+            int row = input.nextInt() - 1;
+            System.out.println("Enter the column destination");
+            int col = input.nextInt() - 1;
 
-
+            if((row >= 2 && row < 6) && (col >= 0 && col < 8))
+            {
+                board.getChessBoard()[row][col] = player;
+                validPlace = true;
+            }
+        }
+        board.printChessboard();
     }
 
     //Do not make alterations to this method!
